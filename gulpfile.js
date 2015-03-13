c12cbc0fd8169511a19cfd05f251aa781bf317f3
Mr-GC <dollars1m@hotmail.com>
2015-03-13 11:32:56 +0000

    Updated gulp file
    Now has seperate servers for dev and dist

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,20 @@ var uglify = require('gulp-uglify'); // minify scripts
 var ngAnnotate = require('gulp-ng-annotate') // allow angular to do dependency injection after script has been minified
 var jshint = require('gulp-jshint'); // js linter
 var stylish = require('jshint-stylish'); // JSHint reporter
+var notify = require("gulp-notify"); // send messages based on Vinyl Files or Errors
 var connect = require('gulp-connect'); // allow app to run on a development server
- 
+var del = require('del'); // delete files and folders - https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+
+// create '.css' file from '.less' 
 gulp.task('less', function() {
-  return gulp.src('css/app.less')
+  return gulp.src(['./app/**/*.less', '!./app/bower_components/**'])
     .pipe(less())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
+// test js files and minify
 gulp.task('js', function () {
-  gulp.src(['src/**/module.js', 'src/**/*.js'])
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
     .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'))
@@ -27,31 +31,66 @@ gulp.task('js', function () {
     .pipe(concat('app.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
-// Run 'less' and 'js' functions to update minified/concatenated js and css files
+// Run 'less' and 'js' functions to update minified/concatenated js and css files after each save
 gulp.task('watch', ['less', 'js'], function() {
-  gulp.watch('css/**/*.less', ['less']);
-  gulp.watch('src/**/*.js', ['js']);
+  gulp.watch('./app/css/**/*.less', ['less']);
+  gulp.watch('./app/js/**/*.js', ['js']);
 });
 
 gulp.task('rev', ['less', 'js'], function() {
   return gulp.src(['dist/**/*.css', 'dist/**/*.js'])
     .pipe(revall())
+    .pipe(gulp.dest('./dist'))
     .pipe(revall.manifest())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(revall.versionFile()) 
+    .pipe(gulp.dest('./dist')); 
 });
 
-gulp.task('compress', function() {
-  gulp.src('lib/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+gulp.task('copy-bower-components', function () {
+  gulp.src('./app/bower_components/**')
+    .pipe(gulp.dest('dist/bower_components'));
 });
 
+gulp.task('copy-html-files', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+// View development 
 gulp.task('connect', function () {
   connect.server({
     root: 'app/',
     port: 8888
   });
 });
+
+gulp.task('connectDist', function () {
+  connect.server({
+    root: 'dist/',
+    port: 9999
+  });
+});
+
+gulp.task('clean', function (cb) {
+  del([
+    'dist/report.csv',
+    // here we use a globbing pattern to match everything inside the 'dist' folder
+    './dist/*',
+    // we don't want to clean this file though so we negate the pattern
+    '!dist/deploy.json'
+  ], cb);
+});
+
+// default task
+gulp.task('default',
+  ['lint', 'connect']
+);
+
+// build task
+gulp.task('build',
+  ['clean', 'less', 'js', 'copy-html-files', 'copy-bower-components', 'connectDist']
+);
